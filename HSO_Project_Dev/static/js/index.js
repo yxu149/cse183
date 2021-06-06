06cd53dfc3ab2059727ee9adfc6cfa1d2194fac8
yxu149 <81794130+yxu149@users.noreply.github.com>
2021-06-05 23:14:16 -0700

    Misc Upload

---
+++ HSO_Project_Dev/static/js/index.js
@@ -0,0 +1,109 @@
+// This will be the object that will contain the Vue attributes
+// and be used to initialize it.
+let app = {};
+
+
+// Given an empty app object, initializes it filling its attributes,
+// creates a Vue instance, and then initializes the Vue instance.
+let init = (app) => {
+
+    // This is the Vue data.
+    app.data = {
+        // Complete as you see fit.
+        query: "",
+        results: [],
+        products: [],
+        login_status: 'out',
+        userName: '',
+        text_status: 'None',   //text_status has two status: None and GET
+    };
+
+    app.enumerate = (a) => {
+        // This adds an _idx field to each element of the array.
+        let k = 0;
+        a.map((e) => {e._idx = k++;});
+        return a;
+    };
+
+
+    app.annotate = (a) => {
+        // This adds an _idx field to each element of the array.
+        let k = 0;
+        a.map((e) => {
+            e._idx = k++;
+            e.fprice = e.price.toLocaleString(
+                undefined, { minimumFractionDigits: 2,
+                    maximumFractionDigits: 2 }
+            );
+        });
+        return a;
+    };
+
+    app.search = function () {
+        axios.get(searchBar_url, {params: {q: app.vue.query}})
+            .then(function(result) {
+                app.vue.results = app.annotate(result.data.results);
+                app.enumerate(app.vue.results)
+            });
+    };
+
+    app.get_username = function () {
+        axios.get(get_username_url)
+            .then(function (result) {
+                app.vue.userName = result.data.userName;
+                if(result.data.userName == ""){
+                    app.vue.login_status = 'out';
+                } else {
+                    app.vue.login_status = 'in';
+                }
+            });
+    };
+
+    app.get_products = function () {
+        axios.get(get_products_url)
+            .then(function (response) {
+                app.vue.products = response.data.products;
+                console.log(app.vue.products);
+            });
+    };
+
+    app.add_wishlist = function (product_id, owner) {
+        axios.post(add_wishlist_url, {
+            product_id: product_id,
+            product_owner: owner,
+        }).then(function (response) {
+
+        });
+    };
+
+    // This contains all the methods.
+    app.methods = {
+        // Complete as you see fit.
+        search: app.search,
+        get_username: app.get_username,
+        get_product: app.get_products,
+        add_wishlist: app.add_wishlist,
+    };
+
+    // This creates the Vue instance.
+    app.vue = new Vue({
+        el: "#vue-target",
+        data: app.data,
+        methods: app.methods
+    });
+
+    // And this initializes it.
+    app.init = () => {
+        // Put here any initialization code.
+        // Typically this is a server GET call to load the data.
+        app.get_username();
+        app.get_products();
+    };
+
+    // Call to the initializer.
+    app.init();
+};
+
+// This takes the (empty) app object, and initializes it,
+// putting all the code i
+init(app);
---
+++ HSO_Project_Dev/static/js/user_profile.js
@@ -0,0 +1,138 @@
+// This will be the object that will contain the Vue attributes
+// and be used to initialize it.
+let app = {};
+
+
+// Given an empty app object, initializes it filling its attributes,
+// creates a Vue instance, and then initializes the Vue instance.
+let init = (app) => {
+
+    // This is the Vue data.
+    app.data = {
+        // Complete as you see fit.
+        user: [],
+        show_items_status: false,
+        items: [],
+        show_community_status: false,
+        posts: [],
+    };
+
+    app.enumerate = (a) => {
+        // This adds an _idx field to each element of the array.
+        let k = 0;
+        a.map((e) => {e._idx = k++;});
+        return a;
+    };
+
+
+    app.annotate = (a) => {
+        // This adds an _idx field to each element of the array.
+        let k = 0;
+        a.map((e) => {
+            e._idx = k++;
+            e.fprice = e.price.toLocaleString(
+                undefined, { minimumFractionDigits: 2,
+                    maximumFractionDigits: 2 }
+            );
+        });
+        return a;
+    };
+
+    app.upload_file = function (event) {
+        console.log(event);
+        console.log(event.target);
+        console.log(event.target.files);
+        let input = event.target;
+        let file = input.files[0];
+        if(file) {
+            let reader = new FileReader();
+            console.log(reader);
+            reader.addEventListener("load", function () {
+                let image = reader.result;
+                axios.post(upload_user_image_url,
+                    {
+                        image: image,
+                    }).then(function () {
+                        app.vue.user.user_image = image;
+                        console.log(app.vue.user.user_image);
+                        app.vue.$forceUpdate()
+                    });
+            });
+            reader.readAsDataURL(file);
+        }
+    }
+
+    app.init_item_status = function () {
+        show_items_status = false;
+        show_community_status = false;
+    }
+
+    app.show_items = function () {
+        axios.get(show_items_url, {params: {user_id: app.vue.user.id}})
+            .then(function (response) {
+                app.vue.show_items_status = !app.vue.show_items_status;
+                if (app.vue.show_community_status == true) {
+                    app.vue.show_community_status = false;
+                }
+                app.vue.items = response.data.products;
+            });
+    }
+
+    app.show_community_post = function () {
+        axios.get(show_community_status_url, {params: {user_id: app.vue.user.id}})
+            .then(function (response) {
+                app.vue.show_community_status=!app.vue.show_community_status;
+                if (app.vue.show_items_status == true) {
+                    app.vue.show_items_status = false;
+                }
+                app.vue.posts = response.data.posts;
+                app.enumerate(app.vue.posts);
+            });
+    }
+
+    app.delete_post = function(row_idx) {
+        let id = app.vue.posts[row_idx].id;
+        axios.get(delete_post_url, {params: {id: id}}).then(function(response) {
+            for(let i = app.vue.posts.length-1; i >= 0; i--) {
+                if(app.vue.posts[i].id === id) {
+                    app.vue.posts.splice(i, 1);
+                    app.enumerate(app.vue.posts);
+                    break;
+                }
+            }
+        });
+    };
+
+    // This contains all the methods.
+    app.methods = {
+        // Complete as you see fit.
+        upload_file: app.upload_file,
+        show_items: app.show_items,
+        show_community_post: app.show_community_post,
+        delete_post: app.delete_post,
+    };
+
+    // This creates the Vue instance.
+    app.vue = new Vue({
+        el: "#vue-target",
+        data: app.data,
+        methods: app.methods
+    });
+
+    // And this initializes it.
+    app.init = () => {
+        // Put here any initialization code.
+        // Typically this is a server GET call to load the data.
+        axios.get(load_Info_url).then(function (response) {
+            app.vue.user = response.data.user;
+        });
+        app.init_item_status();
+    };
+
+    // Call to the initializer.
+    app.init();
+};
+
+// This takes the (empty) app object, and initializes it,
+// putting all the code i
+init(app);
